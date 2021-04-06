5ee098d7ebe4b227a5d1caa7896192e40de222ed
Yafonia Hutabarat <yafonia@gmail.com>
2021-04-06 15:46:51 +0700

    hello world

--- Qwerty/ViewController.swift
+++ Qwerty/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hello world")
         // Do any additional setup after loading the view.
     }
 
