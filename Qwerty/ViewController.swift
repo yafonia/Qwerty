9154e93cba4d9e94c7c2aa60fc8498461d8b881c
Yafonia Hutabarat <yafonia@gmail.com>
2021-04-06 20:35:02 +0700

    halo

--- Qwerty/ViewController.swift
+++ Qwerty/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("wakwa")
+        print("halo")
         // Do any additional setup after loading the view.
     }
 
