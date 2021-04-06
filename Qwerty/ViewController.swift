8936ac6035ea60a52311698db7e0121f65e824a8
Yafonia Hutabarat <yafonia@gmail.com>
2021-04-06 15:55:36 +0700

    coba lagi

--- Qwerty/ViewController.swift
+++ Qwerty/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello world")
+        print("wakwaw")
         // Do any additional setup after loading the view.
     }
 
